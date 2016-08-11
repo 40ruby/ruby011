c2c0a22c6d91615e44a8d18001ad37cf2da41fd8
sho kisaragi <sho@oshiire.to>
2016-08-11 17:18:03 +0900

    added rspec testing modules and tested manage_address

---
+++ manage_address.rb
@@ -0,0 +1,105 @@
+# filename: manage_address.rb
+
+class ManageAddress
+  # 登録済みアドレスリスト
+  attr_reader :addrs
+
+  # データベースの読込・初期化
+  # ファイルが存在していれば内容を読込、なければファイルを新規に作成
+  # == パラメータ
+  # filename:: 読込・または保管先のファイル名
+  # == 返り値
+  # 特になし。但し、@addrs インスタンス変数へ、データベースの内容を保持
+  def initialize(filename)
+    @filename = filename
+    begin
+      if File.exist?(filename)
+        fp = File.open(filename)
+      else
+        fp = File.open(filename, "w+")
+      end
+    rescue => e
+      abort "#{e.class} => #{e.message}"
+    end
+
+    @addrs = fp.each_line.map { |addr|
+      addr.chomp
+    }
+    fp.close
+  end
+
+  # メモリ上のアドレスリストを、ファイルへ保管する
+  # == パラメータ
+  # filename:: 保管先ファイル名。指定がない場合は、初期化時に採用したファイル名
+  def store(filename = @filename)
+    begin
+      fp = File.open(filename, "w")
+    rescue => e
+      abort "#{e.class} => #{e.message}"
+    end
+
+    @addrs.each do |addr|
+      fp.puts(addr)
+    end
+    fp.close
+  end
+
+  # アドレスを新規に登録する。既に登録済みのものであれば登録しない。
+  # == パラメータ
+  # addr:: 登録する IPアドレス
+  # == 返り値
+  # true::  登録できた
+  # false:: 既に同じアドレスあり、登録せず
+  def create(addr)
+    unless @addrs.include?(addr) then
+      @addrs << addr
+      return true
+    else
+      return false
+    end
+  end
+
+  # 登録済みアドレスリストを、配列で返す
+  # == 返り値
+  # array: 登録済みのアドレスリスト
+  def read
+    return @addrs
+  end
+
+  # 登録済みのアドレスを、他のアドレスへ変更する
+  # もし、変更前のものが登録されていなければ、変更前のものも登録する
+  # ただし、変更後のアドレスが既に登録されている場合はエラーを返す
+  # == パラメータ
+  # before:: 既に登録済みのアドレス
+  # after::  変更後のアドレス
+  def update(before, after)
+    if @addrs.include?(after) then
+      return false
+    elsif i = @addrs.index(before) then
+      @addrs[i] = after
+      return true
+    else
+      @addrs << before
+      @addrs << after
+      return true
+    end
+  end
+
+  # 登録済みのアドレスを削除する
+  # == パラメータ
+  # addr: 削除対象のアドレス
+  # == 返り値
+  # string:: 削除された要素
+  # nil::    削除すべき要素が見つからなかったとき
+  def delete(addr)
+    return @addrs.delete(addr)
+  end
+
+  # まだアドレスが登録されていないかどうか
+  # == 返り値
+  # true::  アドレス未登録
+  # false:: 1つ以上のアドレスが登録済み
+  def empty?
+    return @addrs.empty?
+  end
+end
---
+++ spec/manage_address_spec.rb
@@ -0,0 +1,69 @@
+require './manage_address.rb'
+
+describe 'ManageAddress クラス' do
+  before do
+    test       = 'test'
+    @localhost = '127.0.0.1'
+    @addrs     = ['127.0.0.1','192.168.0.100','172.16.10.1']
+    @filename = 'test_db.csv'
+    @hosts = ManageAddress.new(@filename)
+  end
+  describe '#initialize メソッド' do
+    it '内容は空' do
+      expect(@hosts.empty?).to eq(true)
+    end
+  end
+
+  describe '#create メソッド' do
+    it '新規に1つの場合は成功' do
+      expect(@hosts.create('127.0.0.1')).to eq(true)
+    end
+    it '同じアドレスを二度追加すると失敗' do
+      @hosts.create(@localhost)
+      expect(@hosts.create(@localhost)).to eq(false)
+    end
+  end
+
+  describe '#read メソッド' do
+    it '未登録時は空' do
+      expect(@hosts.read).to eq([])
+    end
+    it 'localhost を1つ登録した場合は 127.0.0.1を1つだけ返す' do
+      @hosts.create(@localhost)
+      expect(@hosts.read).to eq([@localhost])
+    end
+    it '複数のアドレスを登録した場合は、それらを返す' do
+      @addrs.each { |host| @hosts.create(host) }
+      expect(@hosts.read).to eq(@addrs)
+    end
+  end
+
+  describe '#update メソッド' do
+    before do
+      @hosts.create(@localhost)
+    end
+    it 'localhostを"192.168.0.1"へ更新する' do
+      expect(@hosts.update(@localhost, '192.168.0.1')).to eq(true)
+      expect(@hosts.read).to eq(['192.168.0.1'])
+    end
+    it '"192.168.0.1"と"172.16.0.1"を追加する' do
+      expect(@hosts.update('192.168.0.1','172.16.0.1')).to eq(true)
+      expect(@hosts.read).to eq([@localhost,'192.168.0.1','172.16.0.1'])
+    end
+    it '更新後のアドレスが既にある場合は失敗' do
+      expect(@hosts.update('192.168.0.1',@localhost)).to eq(false)
+    end
+  end
+
+  describe '#delete メソッド' do
+    before do
+      @hosts.create(@localhost)
+    end
+    it '既に登録済みのアドレスを削除すると、そのアドレスが返る' do
+      expect(@hosts.delete(@localhost)).to eq(@localhost)
+    end
+    it '未登録のアドレスを削除した場合はnil' do
+      expect(@hosts.delete('192.168.0.1')).to eq(nil)
+    end
+  end
+end
